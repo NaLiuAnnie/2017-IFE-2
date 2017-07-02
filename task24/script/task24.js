01a98492c96ab88ba1d42bbcb701f97e22935588
DESKTOP-V1IK2ID\auspiciousna <auspiciousna@163.com>
2017-07-02 22:34:28 +0800

    no message

--- task24/script/task24.js
+++ task24/script/task24.js
@@ -4,18 +4,33 @@
  *
  */
 
+var lNav = document.getElementsByClassName("l-nav")[0];
+var Rtable = document.getElementsByClassName("r-table")[0];
+var tableH = document.getElementById("table-h");
+var tableSubH = tableH.nextElementSibling;
+
+//When a box's position is changed to fixed, It's width will different from other, set it's width same to others.
+function setWidth() {
+    for(var i = 0; i < tableH.children.length; i++) {
+        tableH.children[i].style.width = window.getComputedStyle(tableSubH.children[i]).width;    
+    }
+}
+
 function onScroll() {
-    document.getElementsByClassName("l-nav")[0].style.height = window.innerHeight - document.getElementsByClassName("l-nav")[0].getBoundingClientRect().top + "px";
-    if(document.getElementsByClassName("r-table")[0].getBoundingClientRect().top < 0) {
-        document.getElementById("table-h").style.position = "fixed";
+    lNav.style.height = window.innerHeight - lNav.getBoundingClientRect().top + "px";
+    if(Rtable.getBoundingClientRect().top < 0) {
+        tableH.style.position = "fixed";
+        setWidth();
     }else {
-        document.getElementById("table-h").style.position = "absolute";
+        tableH.style.position = "relative";
     }
 }
 
 function init() {
-    document.getElementsByClassName("l-nav")[0].style.height = window.innerHeight - document.getElementsByClassName("l-nav")[0].getBoundingClientRect().top + "px";
-    window.onscroll = onScroll;
+    lNav.style.height = window.innerHeight - lNav.getBoundingClientRect().top + "px";
+    setWidth();
+    window.addEventListener("scroll", onScroll);
+    window.addEventListener("resize", onScroll);
 }
 
 init();
